5b3e925d8101e46a30e006fa5315020fb9d79fc2
IceR <icerbox@gmail.com>
2022-11-18 15:18:03 +0900

    Добавил таблицу с картинками и DetailViewController

--- project1/project1/ViewController.swift
+++ project1/project1/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  project1
-//
-//  Created by Андрей Антонен on 18.11.2022.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+  
+  var pictures = [String]()
+  
+  var selectedImage: String?
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    let fm = FileManager.default
+    let path = Bundle.main.resourcePath!
+    let items = try! fm.contentsOfDirectory(atPath: path)
+    
+    for item in items {
+      if item.hasPrefix("nssl") {
+        pictures.append(item)
+      }
+    }
+    print(pictures)
   }
 
-
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return pictures.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+    cell.textLabel?.text = pictures[indexPath.row]
+    return cell
+  }
 }
 
