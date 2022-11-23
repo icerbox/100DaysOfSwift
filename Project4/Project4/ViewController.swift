052a9f279008ff592db5672e34b9a1cbc8ab1265
IceR <icerbox@gmail.com>
2022-11-23 17:55:11 +0900

    Выполнил первые два задания четвертого челленджа

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -10,45 +10,62 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
   
+  // Создаем свойство webView для вызова WKWebView
   var webView: WKWebView!
+  // Создаем свойство progressView для вызова UIProgressView
   var progressView: UIProgressView!
+  // Создаем массив websites для хранения разрешенных сайтов
   var websites = ["apple.com", "iltumen.ru", "hackingwithswift.com"]
   
+  
   override func loadView() {
+    // Создаем новый экземпляр WKWEbView и назначаем его свойству webView
     webView = WKWebView()
+    // назначаем делегата себя
     webView.navigationDelegate = self
+    // Назначаем корневому вью вьюконтроллера созданный webview
     view = webView
+    
   }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // Добавляем кнопку в правый верхний угол навигейшн контроллера
     navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-    
+    // Добавляем заполнитель .flexibleSpace чтобы отвести кнопки вправо
     let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
     let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+    let back = UIBarButtonItem(barButtonSystemItem: .rewind, target: webView, action: #selector(webView.goBack))
+    let forward = UIBarButtonItem(barButtonSystemItem: .fastForward, target: webView, action: #selector(webView.goForward))
     progressView = UIProgressView(progressViewStyle: .default)
     progressView.sizeToFit()
     let progressButton = UIBarButtonItem(customView: progressView)
     
-    toolbarItems = [progressButton, spacer, refresh]
+    toolbarItems = [progressButton, spacer, back, forward, refresh]
     navigationController?.isToolbarHidden = false
     
     webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     
+    // Создаем константу url, которому создаем адрес URL из строки которая получена из первого элемента массива websites
     let url = URL(string: "https://" + websites[0])!
+    // Формируем запрос URL из константы url и загружаем его в наш webview
     webView.load(URLRequest(url: url))
+    //
     webView.allowsBackForwardNavigationGestures = true
   }
-
+  
+  // Метод который запускается при нажатии на правую верхнюю кнопку "Open"
   @objc func openTapped() {
+    // объявляем экземпляр UIALertController
     let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
     
+    // Выводим все сайты из массива websites
     for website in websites {
       ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
     }
     
-    
+    // Добавляем кнопку
     ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
     ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
     present(ac, animated: true)
@@ -59,7 +76,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     guard let url = URL(string: "https://" + actionTitle) else { return }
     webView.load(URLRequest(url: url))
   }
-  
+  //
   func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
     title = webView.title
   }
@@ -70,33 +87,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
   }
   
+  
   func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
     // 1. Присваиваем константе url значение URL из навигации
     let url = navigationAction.request.url
     
+    guard navigationAction.navigationType == .other || navigationAction.navigationType == .reload else {
+      decisionHandler(.cancel)
+      let ac = UIAlertController(title: "Заблокировано", message: "Адрес не найден в списке безопасных", preferredStyle: .alert)
+          ac.addAction(UIAlertAction(title: "Закрыть окно", style: .cancel, handler: nil))
+            present(ac, animated: true)
+      return
+    }
     // 2. Мы используем if let для ракскрытия значения опшионала url.host. Это можно прочитать как, если в этом url есть хост, вытащи его. Под хостом имеется в виду домен вебсайта, вроде apple.com. Мы должны раскрыть его, так как не все url содержат
     if let host = url?.host {
-      // 3. Мы проходимся по все сайтам в нашем массиве безопасных сайтов websites.
-      for website in websites {
-        // 4. Мы используем метод строки contains() чтобы проверить содержиться ли текущий хост в проверяемом вебсайте из массива websites
-        if host.contains(website) {
-          // 5. Если текущий хост содержит имя сайта из websites, то разрешаем загрузку
-          decisionHandler(.allow)
-          // 6. После этого производим выход из метода
-          return
-        } else if !host.contains(website) {
-          print("текущий хост: \(host)")
-          let ac = UIAlertController(title: "Warning!", message: "It's url blocked", preferredStyle: .alert)
-          present(ac, animated: true)
-          
-          ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: {
-            (action) in ac.dismiss(animated: true, completion: nil)
-          }))
+          // 3. Проходимся по все сайтам в нашем массиве безопасных сайтов websites.
+          for website in websites {
+          // 4. Используем метод строки contains() чтобы проверить содержиться ли текущий хост в проверяемом вебсайте из массива websites
+            if host.contains(website) {
+              // 5. Если текущий хост содержит имя сайта из websites, то разрешаем загрузку
+              decisionHandler(.allow)
+              // 6. После этого производим выход из метода
+              return
+            }
+          }
         }
-      }
-    }
-    // 7. Если хост не установлен, то мы вызываем decisionHandler с отрицательным результатом и отменяем загрузку
-    decisionHandler(.cancel)
+      // 7. Если хост не установлен, то мы вызываем decisionHandler с отрицательным результатом и отменяем загрузку
+      decisionHandler(.cancel)
   }
 }
 
