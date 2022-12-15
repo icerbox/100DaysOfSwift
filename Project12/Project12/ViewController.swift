33c558a91d700499a5a8bf8b2f7ec582d2e72724
IceR <icerbox@gmail.com>
2022-12-15 13:59:15 +0900

    Выполнил челлендж 11 проекта

---
+++ Project12/Project12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by Андрей Антонен on 15.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
